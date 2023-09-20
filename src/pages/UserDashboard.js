409e10a90eacab44e9b52a3e954eb5db70da209a
RobynSpaulding <robynm27@gmail.com>
2023-09-20 09:21:59 -0700

    adds method in UserDashboard to render the users image and name

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-const-assign */
 import * as React from "react";
 import { useState, useEffect } from "react";
@@ -12,9 +13,26 @@ import findImg from "../images/find.png";
 
 export function UserDashboard() {
   const [showCreateModal, setShowCreateModal] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  const handleIndexUsers = () => {
+    console.log("handleIndexUser");
+    axios.get("http://localhost:3000/users.json").then((response) => {
+      console.log(response.data);
+
+      // Convert the single user object into an array with one element
+      const userArray = [response.data];
+
+      setUsers(userArray);
+    });
+  };
 
   const handleClose = () => setShowCreateModal(false);
   const handleShowCreateModal = () => setShowCreateModal(true);
+
+  useEffect(() => {
+    handleIndexUsers();
+  }, []); // Call handleIndexUsers when the component mounts
 
   return (
     <>
@@ -24,41 +42,52 @@ export function UserDashboard() {
           <Row>
             <Col className="custom-col" md="3">
               <Row style={{ justifyContent: "center" }}>
-                <img className="child-img" src={blankPP}></img>
+                <>
+                  {users.length > 0 ? (
+                    users.map((user, index) => (
+                      <div key={index}>
+                        <img
+                          src={user.profilePicURL}
+                          alt={`Profile Picture of ${user.name}`}
+                          style={{
+                            width: "100px", // Set the desired width here
+                            height: "100px", // Set the desired height here
+                            borderRadius: "50%", // Make it a circle by setting borderRadius to 50%
+                            objectFit: "cover", // Optional: Ensure the image covers the entire container
+                          }}
+                        />
+                        <Col>{user.name}</Col>
+                      </div>
+                    ))
+                  ) : (
+                    <p>Loading...</p> // Show a loading message while data is being fetched
+                  )}
+                </>
               </Row>
-              <Row>
+              {/* <Row>
                 <Col>username</Col>
-              </Row>
+              </Row> */}
             </Col>
             <Col md="8">
               <Row>
                 <Col className="custom-col">
-                  <img src={childImg} style={{ width: "5rem" }}></img>Welcome!
-                  Add or edit children information here.
-                  <Button
-                    className="custom-btn custom-all-btn btn-rounded"
-                    onClick={handleShowCreateModal}
-                  >
+                  <img src={childImg} style={{ width: "5rem" }}></img>Welcome! Add or edit children information here.
+                  <Button className="custom-btn custom-all-btn btn-rounded" onClick={handleShowCreateModal}>
                     Add a Child
                   </Button>
                 </Col>
               </Row>
               <Row>
                 <Col className="custom-col">
-                  <img src={reminderImg} style={{ width: "5rem" }}></img> Set
-                  Reminders to appointments and much more!
-                  <Button className="custom-btn custom-all-btn btn-rounded">
-                    Set reminders
-                  </Button>
+                  <img src={reminderImg} style={{ width: "5rem" }}></img> Set Reminders to appointments and much more!
+                  <Button className="custom-btn custom-all-btn btn-rounded">Set reminders</Button>
                 </Col>
               </Row>
               <Row>
                 <Col className="custom-col">
-                  <img src={findImg} style={{ width: "5rem" }}></img>Find the
-                  best resources to get your questions answered!
-                  <Button className="custom-btn custom-all-btn btn-rounded">
-                    Find resources
-                  </Button>
+                  <img src={findImg} style={{ width: "5rem" }}></img>Find the best resources to get your questions
+                  answered!
+                  <Button className="custom-btn custom-all-btn btn-rounded">Find resources</Button>
                 </Col>
               </Row>
             </Col>
