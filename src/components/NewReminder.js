06a76f764330351239ba7c30f09f7ef21e206b21
RobynSpaulding <robynm27@gmail.com>
2023-10-19 07:19:11 -0700

    added empty 'select category' option to new reminder dropdown

--- src/components/NewReminder.js
+++ src/components/NewReminder.js
@@ -47,6 +47,7 @@ const NewReminder = ({ show, onHide, onSave }) => {
               value={newReminder.category}
               onChange={(e) => setNewReminder({ ...newReminder, category: e.target.value })}
             >
+              <option value="Empty">-- Choose a category --</option>
               <option value="School Event">School Event</option>
               <option value="Medical Appointment">Medical Appointment</option>
               <option value="Extracurricular Activity">Extracurricular Activity</option>
